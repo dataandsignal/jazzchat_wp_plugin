dc8a7bbe017b307bc8d992f6b8556b0e59c21068
Piotr Gregor <piotr@dataandsignal.com>
2020-01-01 22:35:51 +0000

    Fix function names

--- jazzchat.php
+++ jazzchat.php
@@ -13,9 +13,9 @@ Author URI: https://jazzchat.pl
 License: GPLv2 or later
 Date: 1 Jan 2020
 */
-add_action( 'wp_head', 'my_header_scripts' );
+add_action( 'wp_head', 'jazzchat_header_scripts' );
 
-function my_header_scripts(){
+function jazzchat_header_scripts(){
 	
 	$value = get_option( 'jazzchat_uuid' );
 	if( !$value ) {
@@ -26,26 +26,26 @@ function my_header_scripts(){
 	echo sprintf( $format, $value );
 }
 
-add_action( 'admin_menu', 'create_plugin_settings_page' );
+add_action( 'admin_menu', 'jazzchat_create_plugin_settings_page' );
 
-function create_plugin_settings_page() {
+function jazzchat_create_plugin_settings_page() {
 
 	// Add the menu item and page
 	$page_title = 'Jazzchat Settings Page';
 	$menu_title = 'Jazzchat';
 	$capability = 'manage_options';
 	$slug = 'jazzchat';
-	$callback = 'plugin_settings_page_content';
+	$callback = 'jazzchat_plugin_settings_page_content';
 	$icon = 'dashicons-admin-plugins';
 	$position = 100;
 	add_menu_page( $page_title, $menu_title, $capability, $slug, $callback, $icon, $position );
 }
 
-function plugin_settings_page_content() {?>
+function jazzchat_plugin_settings_page_content() {?>
 	<div class="wrap">
 	<h2>Jazzchat Settings</h2><?php
 	if ( isset( $_GET['settings-updated'] ) && $_GET['settings-updated'] ){
-		admin_notice();
+		jazzchat_admin_notice();
 	} ?>
 		<form method="POST" action="options.php">
 		<?php
@@ -57,7 +57,7 @@ function plugin_settings_page_content() {?>
 		</div> <?php
 }
 
-function admin_notice() {
+function jazzchat_admin_notice() {
 	?>
 	<div class="notice notice-success is-dismissible">
 		<p>Your settings have been updated!</p>
@@ -66,24 +66,24 @@ function admin_notice() {
 }
 
 /* Add Settings and Fields */
-add_action( 'admin_init', 'setup_sections' );
-add_action( 'admin_init', 'setup_fields' );
+add_action( 'admin_init', 'jazzchat_setup_sections' );
+add_action( 'admin_init', 'jazzchat_setup_fields' );
 
-function setup_sections() {
-	add_settings_section( 'jazzchat_settings_first_section', 'Domain', 'settings_callback', 'jazzchat' );
+function jazzchat_setup_sections() {
+	add_settings_section( 'jazzchat_settings_first_section', 'Domain', 'jazzchat_settings_callback', 'jazzchat' );
 }
 
-function settings_callback( $arguments ) {
+function jazzchat_settings_callback( $arguments ) {
 	printf( '<div>The only settings you must give to WordPress is Jazzchat\'s domain name that you have registered. If you have not done this yet, you can register domain for free here: <a href=\'https://jazzchat.pl\' target=\'_blank\'>https://jazzchat.pl</a>.</div>' );
 	printf( '<br/><div>You can configure all domain and chat settings in agent\'s panel once you signed into your domain at Jazzchat.</div>' );
 }
 
-function setup_fields() {
-	add_settings_field( 'jazzchat_uuid', 'jazzchat_uuid', 'field_callback', 'jazzchat', 'jazzchat_settings_first_section', '' );
+function jazzchat_setup_fields() {
+	add_settings_field( 'jazzchat_uuid', 'jazzchat_uuid', 'jazzchat_field_callback', 'jazzchat', 'jazzchat_settings_first_section', '' );
 	register_setting( 'jazzchat', 'jazzchat_uuid' );
 }
 
-function field_callback( $arguments ) {
+function jazzchat_field_callback( $arguments ) {
 	
 	$value = get_option( 'jazzchat_uuid' );
 	if( !$value ) {
